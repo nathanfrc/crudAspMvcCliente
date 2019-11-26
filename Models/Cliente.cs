c0c75f4a48036476b6083c7d9398264bb5946b75
Nathan Ferreira Rodrigues Costa <nathan.frc@hotmail.com>
2019-11-26 02:36:31 -0300

    Add files via upload

---
+++ Models/Cliente.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace CrudAspNetMvc.Models
+{
+    [Table("Clientes")]
+    public class Cliente
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Nome { get; set; }
+
+        [Required]
+        [Display(Name = "Documento")]
+        public string Documento { get; set; }
+
+
+        [Required]
+        public string Telefone { get; set; }
+
+        
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime Data { get; set; }
+
+        [Required]
+        public char Tipo { get; set; }
+    }
+}
---
+++ Models/ClienteContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CrudAspNetMvc.Models
+{
+    public class ClienteContext : DbContext
+    {
+        public ClienteContext() : base("Escola")
+        {
+            Database.CreateIfNotExists();
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+
+      //  public System.Data.Entity.DbSet<CrudAspNetMvc.Models.Cliente> Clientes { get; set; }
+    }
+}
